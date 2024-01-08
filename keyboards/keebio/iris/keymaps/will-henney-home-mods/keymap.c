1bb4efaa0df81f497fb50b0f94bb417c20ea5785
William Henney <will@henney.org>
2024-01-08 12:22:59 -0600

    Remove layer switching from home row
    And add G/H as dedicated CAPS_WORD keys on RAISE and LOWER layers

--- keyboards/keebio/iris/keymaps/will-henney-home-mods/keymap.c
+++ keyboards/keebio/iris/keymaps/will-henney-home-mods/keymap.c
@@ -21,31 +21,36 @@
 #define HM_L MT(MOD_RALT, KC_L)
 #define HM_SCLN MT(MOD_RGUI, KC_SCLN)
 
-// dual use of the bottom row layer switch keys
-#define LT_RAISE LT(_RAISE, KC_SPC)
-#define LT_LOWER LT(_LOWER, KC_ENT)
+// 2024-01-08 - I decided dedicated layer keys is better
+/* // dual use of the bottom row layer switch keys */
+/* #define LT_RAISE LT(_RAISE, KC_SPC) */
+/* #define LT_LOWER LT(_LOWER, KC_ENT) */
 
-// and also add more RAISE layer switch keys on the inside column of home row
-//
-#define HM_G LT(_RAISE, KC_G)
-#define HM_H LT(_RAISE, KC_H)
 
-// The reason for making both sides be RAISE is that we can use a
-// combo to get LOWER
+// 2024-01-08 - Remove this failed experiment. It proved to be
+// unnecessary in the end because I never ditched the thumb layer keys
 //
-// The reason that this makes logical sense is that the LOWER layer
-// has mainly shifted versions of keys in the RAISE layer, so we have
-// a combo with the home row SHIFT mod
-enum combos {
-  HJ_LOWER,
-  FG_LOWER,
-};
-const uint16_t PROGMEM hj_combo[] = {KC_H, KC_J, COMBO_END};
-const uint16_t PROGMEM fg_combo[] = {KC_F, KC_G, COMBO_END};
-combo_t key_combos[] = {
-  [HJ_LOWER] = COMBO(hj_combo, MO(_LOWER)),
-  [FG_LOWER] = COMBO(fg_combo, MO(_LOWER)),
-};
+/* // and also add more RAISE layer switch keys on the inside column of home row */
+/* // */
+/* #define HM_G LT(_RAISE, KC_G) */
+/* #define HM_H LT(_RAISE, KC_H) */
+
+/* // The reason for making both sides be RAISE is that we can use a */
+/* // combo to get LOWER */
+/* // */
+/* // The reason that this makes logical sense is that the LOWER layer */
+/* // has mainly shifted versions of keys in the RAISE layer, so we have */
+/* // a combo with the home row SHIFT mod */
+/* enum combos { */
+/*   HJ_LOWER, */
+/*   FG_LOWER, */
+/* }; */
+/* const uint16_t PROGMEM hj_combo[] = {KC_H, KC_J, COMBO_END}; */
+/* const uint16_t PROGMEM fg_combo[] = {KC_F, KC_G, COMBO_END}; */
+/* combo_t key_combos[] = { */
+/*   [HJ_LOWER] = COMBO(hj_combo, MO(_LOWER)), */
+/*   [FG_LOWER] = COMBO(fg_combo, MO(_LOWER)), */
+/* }; */
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -56,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,  GUI_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_ESC, HM_A,    HM_S,    HM_D,    HM_F,    HM_G,                               HM_H,    HM_J,    HM_K,    HM_L,    HM_SCLN, KC_ENTER,
+     KC_ESC, HM_A,    HM_S,    HM_D,    HM_F,    KC_G,                               KC_H,    HM_J,    HM_K,    HM_L,    HM_SCLN, KC_ENTER,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_HOME,          KC_END,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
@@ -70,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_ESC, _______,  KC_LEFT,   KC_UP,  KC_RIGHT, _______,                            _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,
+     KC_ESC, _______,  KC_LEFT,   KC_UP,  KC_RIGHT, CW_TOGG,                            _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      _______, _______, KC_HOME, KC_DOWN, KC_END, _______, KC_LGUI,        KC_RGUI, _______, _______, KC_DQUO, _______, KC_PIPE, KC_DQUO,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
@@ -84,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
     KC_GRAVE,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINUS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-	 KC_DELETE, _______, KC_LEFT, KC_UP, KC_RIGHT, _______,                           _______, KC_MINUS, KC_EQUAL, KC_LBRC, KC_RBRC, _______,
+	 KC_DELETE, _______, KC_LEFT, KC_UP, KC_RIGHT, CW_TOGG,                           _______, KC_MINUS, KC_EQUAL, KC_LBRC, KC_RBRC, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      _______, _______, KC_PGUP, KC_DOWN, KC_PGDN, _______, KC_LALT,        KC_RALT, _______, _______, KC_QUOTE, _______, KC_BSLS, KC_QUOTE, 
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
